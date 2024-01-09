c22b9f26bddbe27d36efe4f512f107cb5375171b
Danylo Hrechyshkin <danil.grechyshkin@sytoss.com>
2024-01-09 17:39:42 +0100

    [master] added graph-ql service

---
+++ graph-ql/src/test/java/graph/server/graphql/GraphQlApplicationTests.java
@@ -0,0 +1,13 @@
+package graph.server.graphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphQlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
